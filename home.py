4085e017668a08222634838141600b64f14db03c
akshara2403 <73586732+akshara2403@users.noreply.github.com>
2025-04-24 21:22:25 -0400

    Update home.py

--- home.py
+++ home.py
@@ -14,9 +14,12 @@ st.markdown("""
 <span style='font-size: 24px; font-weight: bold;'>BA870 Financial Analytics - Final Project</span>
 
 
+st.markdown("""
 <span style='font-size: 18px; font-weight: bold;'>
-Team Members:  
-- Akshara Ramprasad MSDS'25  
-- Samritha Aadhi Ravikumar MSDS'25  
-- Crystal Leatvanich MSBA'25 </span>
+Team Members:<br>
+- Akshara Ramprasad MSDS'25<br>
+- Samritha Aadhi Ravikumar MSDS'25<br>
+- Crystal Leatvanich MSBA'25
+</span>
 """, unsafe_allow_html=True)
+
